1f43fcba3529ceea5df0666268e5dd370bfd1170
Zaman <nur.mnzaman@gmail.com>
2019-04-10 06:52:27 +0600

    Two fragment in a activity

---
+++ app/src/main/java/com/example/fragpractice/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.fragpractice;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void selectFrag(View view) {
+        Fragment fr;
+
+        if(view == findViewById(R.id.button2)) {
+            fr = new FragmentTwo();
+
+        }else {
+            fr = new FragmentOne();
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        FragmentTransaction replace = fragmentTransaction.replace(R.id.fragment_place, fr);
+        fragmentTransaction.commit();
+
+    }
+
+}
+
+
